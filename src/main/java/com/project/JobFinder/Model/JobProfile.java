da04d3f80aeb1d4c16ed387238d810836b307b45
callMeDevMistry <dmistry6487@gmail.com>
2024-06-15 11:36:16 +0530

    commit3

--- src/main/java/com/project/JobFinder/Model/JobProfile.java
+++ src/main/java/com/project/JobFinder/Model/JobProfile.java
@@ -5,17 +5,11 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
 public class JobProfile {
 	
 	public JobProfile() {
-		super();
 	}
 	@Id @GeneratedValue
 	private Long id;
